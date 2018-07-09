444329654430c329cefbbbc3dd8caf7c47630ae7
ridhoemgl <ridhoemgl@gmail.com>
2018-07-10 01:17:10 +0700

    updated function name and removed the hard code

--- moneyCoins.js
+++ moneyCoins.js
@@ -1,4 +1,4 @@
-function converCoin(coin){
+function convertCoin(coin){
 
 	var exchangeArr =[10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 1]
 	var result =[]
@@ -11,51 +11,6 @@ function converCoin(coin){
 		}
 	}
 	return result
-
-	// HARD CODE
-	
-	// var isZero=true
-	// while (isZero){
-	// 	if (coin >= 10000){
-	// 		coin -= 10000
-	// 		result.push(10000)
-	// 	}else if (coin >= 5000){
-	// 		coin -= 5000
-	// 		result.push(5000)
-	// 	}else if (coin >= 2000){
-	// 		coin -= 2000
-	// 		result.push(2000)
-	// 	}else if (coin >= 1000){
-	// 		coin -= 1000
-	// 		result.push(1000)
-	// 	}else if (coin >= 500){
-	// 		coin -= 500
-	// 		result.push(500)
-	// 	}else if (coin >= 200){
-	// 		coin -= 200
-	// 		result.push(200)
-	// 	}else if (coin >= 100){
-	// 		coin -= 100
-	// 		result.push(100)
-	// 	}else if (coin >= 50){
-	// 		coin -= 50
-	// 		result.push(50)
-	// 	}else if (coin >= 20){
-	// 		coin -= 20
-	// 		result.push(20)
-	// 	}else if (coin >= 10){
-	// 		coin -= 10
-	// 		result.push(10)
-	// 	}else{
-	// 		coin -= 1
-	// 		result.push(1)
-	// 	}
-
-	// 	if (coin == 0){
-	// 		isZero = false
-	// 	}
-	// }
-	// return result
 }
 
 console.log(converCoin(543))
